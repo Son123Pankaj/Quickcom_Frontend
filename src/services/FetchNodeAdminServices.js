5073889d9480982ba45d4ec3f628f40adf9cfb8d
pankaj sharma <pankaj.thapak2002@gmail.com>
2026-08-17 16:49:28 +0530

    update new code

--- src/services/FetchNodeAdminServices.js
+++ src/services/FetchNodeAdminServices.js
@@ -1,42 +1,97 @@
-import axios from 'axios'
-const serverURL='https://quickcom-backend-z27h.vercel.app'
-const currentDate=()=>{
-  var d=new Date()
-  var cd=d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate()
-  var ct=d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()
-  return(cd+" "+ct)
-}
-const createDate=(date)=>{
-  var d=new Date(date)
-  var cd=d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate()
-  var ct=d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()
-  return(cd+" "+ct)
-}
-const postData=async(url,body)=>{
-  try{
-  var response= await axios.post(`${serverURL}/${url}`,body)  
-  var result=response.data
-  return result
+import axios from 'axios';
+
+const serverURL = 'https://quickcom-backend-z27h.vercel.app';
+
+const currentDate = () => {
+  const d = new Date();
+
+  const cd =
+    d.getFullYear() +
+    '-' +
+    (d.getMonth() + 1) +
+    '-' +
+    d.getDate();
+
+  const ct =
+    d.getHours() +
+    ':' +
+    d.getMinutes() +
+    ':' +
+    d.getSeconds();
+
+  return cd + ' ' + ct;
+};
+
+const createDate = (date) => {
+  const d = new Date(date);
+
+  const cd =
+    d.getFullYear() +
+    '-' +
+    (d.getMonth() + 1) +
+    '-' +
+    d.getDate();
+
+  const ct =
+    d.getHours() +
+    ':' +
+    d.getMinutes() +
+    ':' +
+    d.getSeconds();
+
+  return cd + ' ' + ct;
+};
+
+const postData = async (url, body) => {
+  try {
+    console.log('POST URL:', `${serverURL}/${url}`);
+    console.log('POST BODY:', body);
+
+    const response = await axios.post(
+      `${serverURL}/${url}`,
+      body
+    );
+
+    console.log('POST RESPONSE:', response.data);
+
+    return response.data;
+  } catch (e) {
+    console.error('POST ERROR:', e);
+
+    return {
+      success: false,
+      message: e.response?.data?.message || e.message,
+      status: e.response?.status
+    };
   }
-  catch(e)
-  {
-    return e.response.data
+};
+
+const getData = async (url) => {
+  try {
+    console.log('GET URL:', `${serverURL}/${url}`);
+
+    const response = await axios.get(
+      `${serverURL}/${url}`
+    );
+
+    console.log('GET RESPONSE:', response.data);
+
+    return response.data;
+  } catch (e) {
+    console.error('GET ERROR:', e);
+
+    return {
+      success: false,
+      message: e.response?.data?.message || e.message,
+      status: e.response?.status
+    };
   }
+};
 
-
-}
-const getData=async(url)=>{
-  try{
-  var response= await axios.get(`${serverURL}/${url}`)  
-  var result=response.data
-  return result
-  }
-  catch(e)
-  {
-    return e.response.data
-  }
-
-
-}
-
-export{ postData,serverURL,currentDate,getData,createDate}
+export {
+  postData,
+  serverURL,
+  currentDate,
+  getData,
+  createDate
+};
